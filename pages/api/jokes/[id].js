805a99d806bef407238d958cb4f22ace4e6636fd
actyvystom <thomas.goerldt@actyvyst.com>
2023-06-07 10:53:47 +0200

    backend read demo end

--- pages/api/jokes/[id].js
+++ pages/api/jokes/[id].js
@@ -1,16 +1,16 @@
-// Import our jokes data from our local data.js file (workaround as we do not know how to connect a db yet)
-import { jokes } from "../../../lib/data.js";
+import dbConnect from "../../../db/connect";
+import Joke from "../../../db/models/Joke";
 
-export default function handler(request, response) {
-  // being a dynamic route, we can deconstruct the id from our query object
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
-  // we use this id to find the joke we want to return in this route
-  const joke = jokes.find((joke) => joke.id === id);
-  if (!joke) {
-    // joke is not within our jokes array, so we set an error status of 404 (resource not found)
-    // and return a message indicating that the joke was not available
-    return response.status(404).json({ status: "Not Found" });
+  if (request.method === "GET") {
+    const joke = await Joke.findById(id);
+    if (!joke) {
+      // joke is not within our jokes array, so we set an error status of 404 (resource not found)
+      // and return a message indicating that the joke was not available
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(joke);
   }
-  // if we have a joke object, we set our response with a OK http status code and return it as json
-  response.status(200).json(joke);
 }
--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -1,7 +1,10 @@
-// Import our jokes data from our local data.js file (workaround as we do not know how to connect a db yet)
-import { jokes } from "../../../lib/data.js";
+import dbConnect from "../../../db/connect";
+import Joke from "../../../db/models/Joke";
 // each api route can have exactly one handler function (explicitly named handler)
-export default function handler(request, response) {
-  // set the http status code to 200 and deliver our data directly as JSON
-  response.status(200).json(jokes);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const jokes = await Joke.find();
+    response.status(200).json(jokes);
+  }
 }
